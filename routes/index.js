559b8419469ed9866feedb5677e3e515d5e5bc1a
adadesions <adadesions@gmail.com>
2020-01-30 14:55:59 +0700

    Completed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/analysis', function(req, res, next) {
+  let seq = req.body.seq
+  let seqLen = seq.length
+  let bases = {A: 0, T:0, C:0, G:0}
+
+  for(let s of seq) {
+    bases[s] += 1
+  }
+
+  const percent = (x, y) => (x*100/y).toFixed(2)
+
+  bases.A = percent(bases.A, seqLen)
+  bases.T = percent(bases.T, seqLen)
+  bases.C = percent(bases.C, seqLen)
+  bases.G = percent(bases.G, seqLen)
+
+  res.render('analysis', bases)
+});
+
 module.exports = router;
